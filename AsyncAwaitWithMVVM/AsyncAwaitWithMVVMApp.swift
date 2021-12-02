62cb9dd869aaadd59ab75fa3bc2477c192a4ec77
pradeepiOS <pradeep.iostech@gmail.com>
2021-12-02 09:55:02 +0530

    Initial Commit

---
+++ AsyncAwaitWithMVVM/AsyncAwaitWithMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitWithMVVMApp.swift
+//  AsyncAwaitWithMVVM
+//
+//  Created by Pradeep's Macbook on 02/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitWithMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncAwaitWithMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncAwaitWithMVVM
+//
+//  Created by Pradeep's Macbook on 02/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
